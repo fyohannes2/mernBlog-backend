8c0906d0097725e0373c5f188aa4f0c4e70e603a
Filmon <filmon@Lydias-Air.attlocal.net>
2022-06-21 14:35:27 -0400

    created register page.

---
+++ routes/userRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+const auth = require('../middleware/authMiddleware');
+
+const {
+    registerUser,
+    loginUser,
+    getProfile
+} = require('../controllers/userController')
+
+router.post('/register', registerUser)
+
+router.post('/login', loginUser);
+
+router.get('/profile', [auth], getProfile)
+
+module.exports = router; 
